b8f8949bb8634d787a81dbc63acbbaa27e1d4dd1
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-05-18 18:17:53 +0200

    fix motd

--- src/main/java/net/grexcraft/cloud_bungee/listener/ProxyPingListener.java
+++ src/main/java/net/grexcraft/cloud_bungee/listener/ProxyPingListener.java
@@ -1,15 +1,26 @@
 package net.grexcraft.cloud_bungee.listener;
 
 import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 public class ProxyPingListener implements Listener {
-    @EventHandler
+    private final String motd = "          &7----[&6&lGrex&c&lCraft &81.8-1.19&7&7]----&r\\n          &7TTT, Skyblock, KitPvP and more...";
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPing(ProxyPingEvent e){
         ServerPing serverPing = e.getResponse();
-        serverPing.setDescription("          &7----[&6&lGrex&c&lCraft &81.8-1.19&7&7]----&r\\n          &7TTT, Skyblock, KitPvP and more...");
+        String message = replaceToMinecraftStringPlaceholders(motd);
+        serverPing.setDescriptionComponent(new TextComponent(message));
         e.setResponse(serverPing);
     }
+
+    private String replaceToMinecraftStringPlaceholders(String motd) {
+        return motd
+                .replace("&", "§")
+                .replace("\\n", "\n");
+    }
+
 }
